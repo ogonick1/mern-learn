e3a69ba54f102eec1a2107f82d3972b0a73e082d
Ogonick <ogonick1@gmail.com>
2022-12-27 19:43:00 +0200

    fix

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('express-async-errors');
 const config = require('config');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 const { errorCodes } = require('./errors/error-codes');
 const { GeneralError } = require('./errors');
@@ -9,6 +10,7 @@ const { GeneralError } = require('./errors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', routes);
 
 app.use((err, req, res, next) => {
