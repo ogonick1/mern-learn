c5759d6c9ce75b1bec8984300e4a4270132f7230
Ogonick <ogonick1@gmail.com>
2023-01-10 10:02:07 +0200

    t

--- ui/src/plugins/store/carSlice.js
+++ ui/src/plugins/store/carSlice.js
@@ -1,13 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import CarBrandService from '../../services/carBrand.service';
-
-export const search = createAsyncThunk(
-  'cars/search',
-  async (param) => {
-    const result = await CarBrandService.search(param);
-    return result.carBrands;
-  },
-);
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   carBrands: null,
@@ -18,13 +9,6 @@ export const carSlice = createSlice({
   name: 'cars',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(search.pending, (state) => { state.loading = true; });
-    builder.addCase(search.fulfilled, (state, action) => {
-      state.loading = false;
-      state.carBrands = action.payload;
-    });
-  },
 });
 
 export default carSlice.reducer;
--- ui/src/services/carBrand.service.js
+++ ui/src/services/carBrand.service.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default class CarBrandService {
   static search(params) {
-    return axios.post('car-brand/search', { params });
+    return axios.post('car-brand/search', params);
   }
 }
