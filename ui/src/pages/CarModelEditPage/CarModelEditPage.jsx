c3468880ba25526013d7dfbf11cdc1ee8052b8b3
prozhok <prozhok@ascendix.net>
2023-01-18 00:11:36 +0200

    fix

--- ui/src/pages/CarModelEditPage/CarModelEditPage.jsx
+++ ui/src/pages/CarModelEditPage/CarModelEditPage.jsx
@@ -1,7 +1,7 @@
 import {
-  TextField, Typography, Box, Stack, Button, Container, Autocomplete,
+  TextField, Typography, Box, Stack, Button, Container,
 } from '@mui/material';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, Controller, useFieldArray } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -11,6 +11,13 @@ import { CarModelService } from '../../services/carModel.service';
 import { CustomSelect } from '../../components/fields/CustomSelect';
 import { CarBrandService } from '../../services/carBrand.service';
 import { ExtraFeatureService } from '../../services/extraFeature.service';
+
+const bodyTypeEnum = [
+  'SEDAN',
+  'STATION_WAGON',
+  'HATCHBACK',
+  'COUPE',
+];
 
 const getCarModelOptions = () => {
   return CarBrandService.search()
@@ -30,6 +37,12 @@ export const CarModelEditPage = () => {
   const { id } = useParams();
   const { t } = useTranslation();
   const navigate = useNavigate();
+
+  const bodyTypeOptions = bodyTypeEnum.map((value) => ({
+    title: t(`enum.bodyType.${value}`),
+    value,
+  }));
+
   const schema = yup
     .object()
     .shape({
@@ -40,22 +53,51 @@ export const CarModelEditPage = () => {
       brandOption: yup.object()
         .required(t('validationErrors.required')),
       extraFeaturesOptions: yup.array(),
+      powerUnits: yup.array(),
+      bodyTypes: yup.array(),
     });
 
   const {
     handleSubmit,
     control,
-    reset,
     formState: {
-      errors,
       isDirty,
       isValid,
       isSubmitted,
     },
   } = useForm({
+    defaultValues: {
+      powerUnits: [
+        {
+          engineVolume: 1600,
+          fuelType: 'GAS_PETROL',
+          gearBox: 'AUTOMATIC',
+          driveType: 'BACK',
+        },
+      ],
+    },
     mode: 'all',
     resolver: yupResolver(schema),
   });
+
+  const {
+    fields: powerUnitsFields,
+    append: appendPowerUnit,
+    remove: removePowerUnit,
+  } = useFieldArray({
+    control,
+    name: 'powerUnits',
+  });
+
+  const addPowerUnit = () => {
+    appendPowerUnit({
+      engineVolume: 1600,
+      fuelType: 'GAS_PETROL',
+      gearBox: 'AUTOMATIC',
+      driveType: 'BACK',
+    });
+  };
+
   const createCarModel = async (form) => {
     const model = {
       name: form.name,
@@ -63,20 +105,19 @@ export const CarModelEditPage = () => {
       extraFeaturesIds: form.extraFeaturesOptions.map(({ _id }) => _id),
       yearStart: 1960,
       yearEnd: 2020,
-      powerUnits: [{
-        engineVolume: 1600,
-        fuelType: 'GAS_PETROL',
-        gearBox: 'AUTOMATIC',
-        driveType: 'BACK',
-      }],
-      bodyTypes: ['SEDAN'],
+      powerUnits: form.powerUnits,
+      // powerUnits: form.powerUnits.map((powerUnit) => ({
+      //   engineVolume: powerUnit.engineVolume,
+      //   fuelType: powerUnit.fuelType.value,
+      //   gearBox: powerUnit.gearBox.value,
+      //   driveType: powerUnit.driveType.value,
+      // })),
+      bodyTypes: form.bodyTypes.map(({ value }) => value),
     };
 
-    console.log(model);
-
     try {
-      // await CarModelService.create(model);
-      // navigate('/car-model');
+      await CarModelService.create(model);
+      navigate('/car-model');
     } catch (error) {
       toast.error(error?.response?.data?.message);
     }
@@ -95,7 +136,6 @@ export const CarModelEditPage = () => {
         >
           <Typography variant="h4" component="h4">{t('carModel.title')}</Typography>
           <form className="form" onSubmit={handleSubmit(onSubmit)}>
-
             <Controller
               control={control}
               name="name"
@@ -178,6 +218,165 @@ export const CarModelEditPage = () => {
                 );
               }}
             />
+            <br />
+            <br />
+            <br />
+            <Controller
+              control={control}
+              name="bodyTypes"
+              render={({
+                field: {
+                  onChange, onBlur, value,
+                },
+                fieldState: { error },
+              }) => {
+                const isFieldValid = error ? false : undefined;
+                const errorText = isFieldValid === false ? error?.message : '';
+                return (
+                  <CustomSelect
+                    multiple
+                    options={bodyTypeOptions}
+                    id="bodyTypes"
+                    label="bodyTypes"
+                    getOptionLabel={(option) => option.title || ''}
+                    isOptionEqualToValue={(option, val) => val.value === option.value}
+                    onChange={onChange}
+                    value={value || []}
+                  />
+                );
+              }}
+            />
+            <br />
+            <br />
+            <br />
+            {powerUnitsFields.map((powerUnitsField, index) => {
+              return (
+                <div key={powerUnitsField.id}>
+                  <hr />
+                  <Controller
+                    control={control}
+                    name={`powerUnits.${index}.engineVolume`}
+                    render={({
+                      field: {
+                        onChange, onBlur, value,
+                      },
+                      fieldState: { error },
+                    }) => {
+                      const isFieldValid = error ? false : undefined;
+                      const errorText = isFieldValid === false ? error?.message : '';
+                      return (
+                        <TextField
+                          // TODO
+                          // InputLabelProps={{ shrink: value }}
+                          onChange={onChange}
+                          onBlur={onBlur}
+                          value={value || ''}
+                          id={`powerUnits.${index}.engineVolume`}
+                          label="engineVolume"
+                          variant="outlined"
+                          margin="normal"
+                          helperText={errorText || ''}
+                          error={!!errorText}
+                        />
+                      );
+                    }}
+                  />
+                  <br />
+                  <br />
+                  <Controller
+                    control={control}
+                    name={`powerUnits.${index}.fuelType`}
+                    render={({
+                      field: {
+                        onChange, onBlur, value,
+                      },
+                      fieldState: { error },
+                    }) => {
+                      const isFieldValid = error ? false : undefined;
+                      const errorText = isFieldValid === false ? error?.message : '';
+                      return (
+                        <CustomSelect
+                          id={`powerUnits.${index}.fuelType`}
+                          label="fuelType"
+                          options={[
+                            'PETROL',
+                            'DIESEL',
+                            'GAS',
+                            'HYBRID',
+                            'ELECTRO',
+                            'GAS_PETROL',
+                          ]}
+                          // getOptionLabel={(option) => option.title || ''}
+                          onChange={onChange}
+                          value={value || null}
+                        />
+                      );
+                    }}
+                  />
+                  <br />
+                  <br />
+                  <Controller
+                    control={control}
+                    name={`powerUnits.${index}.gearBox`}
+                    render={({
+                      field: {
+                        onChange, onBlur, value,
+                      },
+                      fieldState: { error },
+                    }) => {
+                      const isFieldValid = error ? false : undefined;
+                      const errorText = isFieldValid === false ? error?.message : '';
+                      return (
+                        <CustomSelect
+                          id={`powerUnits.${index}.gearBox`}
+                          label="gearBox"
+                          options={[
+                            'MECHANICAL',
+                            'AUTOMATIC',
+                          ]}
+                          onChange={onChange}
+                          value={value || null}
+                        />
+                      );
+                    }}
+                  />
+                  <br />
+                  <br />
+                  <Controller
+                    control={control}
+                    name={`powerUnits.${index}.driveType`}
+                    render={({
+                      field: {
+                        onChange, onBlur, value,
+                      },
+                      fieldState: { error },
+                    }) => {
+                      const isFieldValid = error ? false : undefined;
+                      const errorText = isFieldValid === false ? error?.message : '';
+                      return (
+                        <CustomSelect
+                          id={`powerUnits.${index}.driveType`}
+                          label="driveType"
+                          options={[
+                            'FRONT',
+                            'BACK',
+                            'FULL',
+                          ]}
+                          onChange={onChange}
+                          value={value || null}
+                        />
+                      );
+                    }}
+                  />
+                  {powerUnitsFields.length !== 1 && (
+                    <Button onClick={() => removePowerUnit(index)}>
+                      Remove
+                    </Button>
+                  )}
+                </div>
+              );
+            })}
+            <Button onClick={addPowerUnit}>Add Power Unit</Button>
             <Stack
               marginTop={2}
               spacing={5}
